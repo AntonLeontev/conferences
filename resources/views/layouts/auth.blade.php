0f9382fc364a61a9ca86e1f36fe718dec1dd0d89
Anton <aner-anton@yandex.ru>
2024-02-01 15:33:34 +0300

    add: favicon

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,20 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-
 <head>
-    <title>@yield('title') | {{ config('app.name') }}</title>
-    <meta charset="UTF-8">
-    <meta name="format-detection" content="telephone=no">
-    <link rel="shortcut icon" href="favicon.ico">
-    <meta name="robots" content="noindex, nofollow">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	@include('partials.head')
 
-    @vite(['resources/css/style.css', 'resources/js/app.js'])
+    <title>@yield('title') | {{ config('app.name') }}</title>
 
     @yield('head_scripts')
-	<style>
-		[x-cloak] {display: none;}
-	</style>
 	<script src="/js/scripts.js" defer></script>
 </head>
 
---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,16 @@
+<meta charset="UTF-8">
+<meta name="format-detection" content="telephone=no">
+<link rel="shortcut icon" href="favicon.ico">
+<meta name="robots" content="noindex, nofollow">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+@vite(['resources/css/style.css', 'resources/js/app.js'])
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+
+<style>
+	[x-cloak] {display: none;}
+</style>
